99fdb18d8342f3f665644204aa38e09af799134c
oseiassuquina <suquina10@gmail.com>
2020-06-17 21:57:11 -0300

    socorro

---
+++ GetHelp_Cadastrar/GetHelp_Cadastrar/Data/PacienteContext.cs
@@ -0,0 +1,22 @@
+﻿using GetHelp_Cadastrar.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetHelp_Cadastrar.Data
+{
+    public class PacienteContext : DbContext
+    {
+        public DbSet<Paciente> Pacientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=Pacientes;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+
+        }
+    }
+}
---
+++ GetHelp_Cadastrar/GetHelp_Cadastrar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GetHelp_Cadastrar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
